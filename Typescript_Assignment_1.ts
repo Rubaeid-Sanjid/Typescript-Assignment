6937f1115f8f6ff1717e0a7b72aa6970c6d36192
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-05-13 20:17:57 +0600

    solved problem 2

--- Typescript_Assignment_1.ts
+++ Typescript_Assignment_1.ts
@@ -1,13 +1,25 @@
 {
-  function formatString(input: string, toUpper?: boolean = true): string {
-    if (toUpper) {
-      console.log(input.toUpperCase());
+  function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === true || toUpper === undefined) {
+      return input.toUpperCase();
     } else {
-      console.log(input.toLowerCase());
+      return input.toLowerCase();
     }
   }
 
-  formatString("Hello"); // Output: "HELLO"
-  formatString("Hello", true); // Output: "HELLO"
-  formatString("Hello", false); // Output: "hello"
+  function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    const filteredItems = items.filter((item : {title : String; rating : number}) => item.rating >= 4);
+    return filteredItems;
+  }
+  const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book B", rating: 4.00 },
+  { title: "Book C", rating: 5.0 }
+];
+
+const result = filterByRating(books);
+console.log(result);
+
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
 }
